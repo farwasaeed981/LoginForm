601114d460784523edbeb6c677aa297c09917907
farwasaeed981 <111896641+farwasaeed981@users.noreply.github.com>
2023-05-31 23:18:21 +0500

    Create form.js

---
+++ form.js
@@ -0,0 +1,72 @@
+const form = document.getElementById("form");
+const username = document.getElementById("user");
+const email = document.getElementById("email");
+const password = document.getElementById("pass");
+const password2 = document.getElementById("confpass");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const isValidEmail = (email) => {
+  const sym =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return sym.test(String(email).toLowerCase());
+};
+
+const validateInputs = () => {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+  if (usernameValue === "") {
+    setError(username, "Username is required");
+  } else {
+    setSuccess(username);
+  }
+
+  if (emailValue === "") {
+    setError(email, "Email is required");
+  } else if (!isValidEmail) {
+    setError(email, "Please provide a valid email");
+  } else {
+    setSuccess(email);
+  }
+  
+  if(passwordValue===''){
+    setError(password, 'Password is required');
+  } else if(passwordValue.length < 8){
+    setError(password, 'password must be upto 8 characters');
+}
+    else {
+        setSuccess(password);
+    }
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
+}
